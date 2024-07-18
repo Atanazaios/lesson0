d74d9c3df0d02cb91d3eccffc481c3b96ad97d3d
Nikolos <Vertikon377@gmail.com>
2024-07-18 12:24:53 +0300

    Initial commit

---
+++ Homework2.py
@@ -0,0 +1,4 @@
+lesson_count = 12
+hours_spent = 1.5
+First_name = 'Python'
+print('Курс:',First_name,',' 'всего задач:',lesson_count,',' 'затрачено часов:',hours_spent,',','среднее время выполнения', hours_spent / lesson_count )
